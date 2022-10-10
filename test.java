bb18a290fd821d7e88a3869d114159f3fb864531
Robel-robel <robelberhanu89@gmail.com>
2022-10-10 19:58:51 +0200

    added test file

---
+++ test.java
@@ -0,0 +1,24 @@
+import java.util.regex.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class test{
+
+public static void main(String[] args){
+    final String str = "<tag>apple</tag><b>hello</b><tag>orange</tag><tag>pear</tag>";
+    System.out.println(Arrays.toString(getTagValues(str).toArray())); // Prints [apple, orange, pear]
+}
+
+private static final Pattern TAG_REGEX = Pattern.compile("<tag>(.+?)</tag>", Pattern.DOTALL);
+
+private static List<String> getTagValues(final String str) {
+    final List<String> tagValues = new ArrayList<String>();
+    final Matcher matcher = TAG_REGEX.matcher(str);
+    while (matcher.find()) {
+        tagValues.add(matcher.group(1));
+    }
+    return tagValues;
+}
+
+}
